ad190f2ae27c28c4c889f19072d9521244747804
sealx017 <slsouvik@gmail.com>
2025-07-11 14:28:04 -0400

    New datasets

--- R/data.R
+++ R/data.R
@@ -11,3 +11,12 @@
 #' @source \url{https://aacrjournals.org/cancerres/article/78/20/5970/631815/Spatially-Resolved-Transcriptomics-Enables}
 #' @usage data(mela_coords)
 "mela_coords"
+
+#' SpaceBF_melanoma dataset
+#'
+#' The full melanoma dataset, consisting gene expression, xy-coordinates, and LR pairs
+#'
+#' @format A data frame with N = 292 rows (spots, 1 removed based on QC) and 3 columns (xy coordinates and celltypes):
+#' @source \url{https://aacrjournals.org/cancerres/article/78/20/5970/631815/Spatially-Resolved-Transcriptomics-Enables}
+#' @usage data(mela_coords)
+"SpaceBF_melanoma"
