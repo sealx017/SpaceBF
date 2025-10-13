2f23b3c42b75c36afb44ad5c9fdd82a97e05300b
sealx017 <slsouvik@gmail.com>
2025-10-13 12:14:08 -0400

    ICAR update

--- R/data.R
+++ R/data.R
@@ -6,34 +6,34 @@
 #' \describe{
 #'   \item{x}{x co-ordinate of the cell}
 #'   \item{y}{y co-ordinate of the cell}
-#'   \item{celltypes}{Celltypes detected by the RCTD software based on the gene expression profile}
+#'   \item{celltypes}{Celltypes detected by RCTD}
 #' }
 #' @source \url{https://aacrjournals.org/cancerres/article/78/20/5970/631815/Spatially-Resolved-Transcriptomics-Enables}
 #' @usage data(mela_coords)
-#' @export
+#' @keywords datasets
 "mela_coords"
 
 #' SpaceBF_melanoma dataset
 #'
-#' The full melanoma dataset, consisting gene expression, xy-coordinates, and LR pairs
+#' The full melanoma dataset: gene expression, xy-coordinates, and LR pairs
 #'
-#' @format A list of three data frames, with gene expression, co-ordinates, and LR pairs
+#' @format A list with components:
 #' \describe{
 #'   \item{gene_data_mela}{gene expression data}
 #'   \item{coords_data_mela}{xy co-ordinates}
-#'   \item{LR_pairs}{161 LR pairs}
+#'   \item{LR_pairs}{161 ligand–receptor pairs}
 #' }
 #' @source \url{https://aacrjournals.org/cancerres/article/78/20/5970/631815/Spatially-Resolved-Transcriptomics-Enables}
 #' @usage data(SpaceBF_melanoma)
-#' @export
+#' @keywords datasets
 "SpaceBF_melanoma"
 
 #' SpaceBF_cSCC dataset
 #'
-#' The full cSCC dataset, consisting gene expression, xy-coordinates, and keratin pairs
+#' The full cSCC dataset: gene expression, xy-coordinates, and keratin pairs
 #'
-#' @format A list of three data frames, with gene expression, co-ordinates, and keratin pairs
+#' @format A list of three data frames
 #' @source \url{https://www.cell.com/cell/fulltext/S0092-8674(20)30672-3}
 #' @usage data(SpaceBF_cSCC)
-#' @export
+#' @keywords datasets
 "SpaceBF_cSCC"
